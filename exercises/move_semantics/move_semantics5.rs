c712c8241213991116b25fda43f3b12a715edd47
Pedrokielma <pedrokielma@Pedros-MBP.home>
2023-04-09 15:59:42 +0100

    modules

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,17 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let mut x = 100;
-    let y = &mut x;
-    let z = &mut x;
-    *y += 100;
+    let x = 100;
+    let mut y = x;
+    
+    println!("print y {}", y);
+    let z = &mut y;
+    println!("print z {}", z);
     *z += 1000;
-    assert_eq!(x, 1200);
+    println!("print z {}", z);
+    println!("print x {}", x);
+    assert_eq!(y, 1100);
 }
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,25 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let string = input.trim().to_string();
+
+  return string
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+   let newString = input.to_owned() + &" world!".to_string();
+   return newString
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let new_string = input.replace("cars", "balloons");
+    return new_string;
 }
 
 #[cfg(test)]
